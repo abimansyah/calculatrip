73d4978013524030d8c8db432eed89b151352cb1
Rodhey48 <rinter18@gmail.com>
2022-02-17 01:37:36 +0700

    create endpoint user

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+const {
+  hasPassword
+} = require('../helpers/bcrypt')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -14,13 +17,72 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    username: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'Email must be unique'
+      },
+      validate: {
+        notEmpty: {
+          msg: "Email is required"
+        },
+        notNull: {
+          msg: "Email is required"
+        },
+        isEmail: {
+          msg: "Invalid email format"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Password is required"
+        },
+        notNull: {
+          msg: "Password is required"
+        },
+        len: {
+          args: [7, 100],
+          msg: "Password length min 7"
+        }
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'Username  must be unique'
+      },
+      validate: {
+        notEmpty: {
+          msg: "Username is required"
+        },
+        notNull: {
+          msg: "Username is required"
+        },
+        len: {
+          args: [6, 100],
+          msg: "Username must have more than 6 characters"
+        },
+        is: {
+          args: ["^[a-z]+$", 'i'],
+          msg: "username can not contain symbols"
+        },
+      }
+    },
     avatar: DataTypes.STRING,
     phoneNumber: DataTypes.STRING,
     birthDate: DataTypes.DATE
   }, {
+    hooks: {
+      beforeCreate(inst, opt) {
+        inst.password = hasPassword(inst.password)
+      }
+    },
     sequelize,
     modelName: 'User',
   });
