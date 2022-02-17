b0cf71c2488bd04ff1a542769a6c5c7daa4f862e
Rodhey48 <rinter18@gmail.com>
2022-02-17 19:21:26 +0700

    fix router api doc user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,8 +10,8 @@ const authentication = require("../middlewares/Authentication")
 userRouter.get('/', authentication, UserController.getUsers)
 userRouter.post('/login', UserController.loginUser)
 userRouter.post('/register', UserController.registerUser)
-userRouter.get('/:input', authentication, UserController.getUserByInput)
 userRouter.get('/profile', authentication, UserController.getUserProfile)
+userRouter.get('/profile/:input', authentication, UserController.getUserByInput)
 userRouter.put('/:id', authentication, UserController.editUser)
 
 module.exports = userRouter
