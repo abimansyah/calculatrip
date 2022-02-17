f04b3312bc39b6f5308ff3d86a29741e4dff5a5c
unknown <abimansyahdwitama@gmail.com>
2022-02-17 20:05:02 +0700

    delete

---
+++ server/services/app/app.js
@@ -0,0 +1,21 @@
+function likes(names) {
+  // TODO
+  if(names.length === 0) {
+    return "no one likes this"
+  } else if (names.length >0 && names.length <=1) {
+    return `${names[1]} likes this`
+  } else if (names.length >0 && names.length <=2) {
+    return `${names[1]}, ${names[2]} and ${names[3]} likes this`
+  } else {
+    return `${names[1]}, ${names[2]} and ${names.slice(2).length} likes this`
+  }
+}
+describe('example tests', function() {
+  it('should return correct text', function() {
+    Test.assertEquals(likes([]), 'no one likes this');
+    Test.assertEquals(likes(['Peter']), 'Peter likes this');
+    Test.assertEquals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+    Test.assertEquals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+    Test.assertEquals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+  });
+});
