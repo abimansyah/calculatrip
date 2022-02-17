c57c6996c3113dc57391fc166e37c43bc6e361af
unknown <abimansyahdwitama@gmail.com>
2022-02-18 01:02:32 +0700

    feat: CRUD on trip controller done

--- server/routes/tripRoutes.js
+++ server/routes/tripRoutes.js
@@ -9,5 +9,8 @@ const authentication = require("../middlewares/Authentication")
 
 tripRouter.get('/', authentication, TripController.getTrips)
 tripRouter.post('/', authentication, TripController.postTrip)
+tripRouter.get('/:id', authentication, TripController.getTripById)
+tripRouter.delete('/:id', authentication, TripController.deleteTrip)
+tripRouter.put('/:id', authentication, TripController.editTrip)
 
 module.exports = tripRouter
