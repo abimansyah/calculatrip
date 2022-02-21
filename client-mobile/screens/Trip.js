140eb57cbe7bdadea5769e65d3194a933146454c
Riki Rifaldi <rkrfld@gmail.com>
2022-02-21 23:49:06 +0700

    finish fetch companiopn

--- client-mobile/screens/Trip.js
+++ client-mobile/screens/Trip.js
@@ -229,7 +229,7 @@ export default function Trip({ route }) {
           </SafeAreaView>
         </TouchableWithoutFeedback>
       </ScrollView >
-        <BottomTab data={{data: trip.id}}/>
+        <BottomTab data={trip.id}/>
     </>
   );
 }
