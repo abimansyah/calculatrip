e1fdd83352b535bf1c240724633c64051d12527e
arumdyaharum <arumdyaharum@gmail.com>
2022-02-21 23:50:05 +0700

    install loading tapi gak jadi

--- client-mobile/screens/Login.js
+++ client-mobile/screens/Login.js
@@ -14,6 +14,7 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import Load from "react-native-loading-gif";
 import { styles } from '../styles';
 import logo from '../assets/logo.png'
 
@@ -58,7 +59,6 @@ export default function Login({ navigation }) {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <SafeAreaView style={styles.mainContainer}>
         <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={keyboardVerticalOffset}>
-
           <View style={
             {
               height: '55%'
